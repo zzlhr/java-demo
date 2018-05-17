9265828ba9f83167bb6448f66fec402f4eb4e842
liuhaoran <lhr@lhrsite.com>
2018-05-17 14:20:12 +0800

    .

---
+++ annontation/src/main/java/com/lhrsite/demo/Application.java
@@ -0,0 +1,18 @@
+package com.lhrsite.demo;
+
+/**
+ * 描述
+ *
+ * @author lhr
+ * @data 2018/5/17
+ * @site https://www.lhrsite.com
+ */
+@Author
+public class Application {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ annontation/src/main/java/com/lhrsite/demo/Author.java
@@ -0,0 +1,11 @@
+package com.lhrsite.demo;
+
+/**
+ * 描述
+ *
+ * @author lhr
+ * @data 2018/5/17
+ * @site https://www.lhrsite.com
+ */
+public @interface Author {
+}
